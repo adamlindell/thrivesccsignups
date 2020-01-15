90c266c0ec6cded07b00a74f88d04b2b0546bb00
Adam Lindell <adam.lindell@gmail.com>
2020-01-14 22:24:48 -0600

    changed initial message for testing

--- signup/index.js
+++ signup/index.js
@@ -10,7 +10,7 @@ module.exports = async function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a name on the query string or in the request monkey"
         };
     }
 };
